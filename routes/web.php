80b90bf7fd93bd2300e493f05c0f3017f50f51e2
estherfuraha <estherbyamasufuraha@gmail.com>
2020-12-16 03:26:18 -0800

    add route caisse_utilisateurs

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:manage-users')->group(function(){
     Route::resource('users','UsersController');
 });
+
+Route::get('caisse_utilisateurs','Caisse_utilisateursController@index');
+Route::get('caisse_utilisateurs/create','Caisse_utilisateursController@create');
+Route::post('caisse_utilisateurs','Caisse_utilisateursController@store');
+Route::get('caisse_utilisateurs/edit/{caisse_utilisateur}','Caisse_utilisateursController@edit');
+Route::put('caisse_utilisateurs/{caisse_utilisateur}','Caisse_utilisateursController@update');
+Route::post('caisse_utilisateurs/destroy/{caisse_utilisateur}','Caisse_utilisateursController@destroy');
