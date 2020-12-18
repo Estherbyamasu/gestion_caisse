b70a3d2d8cc7867308dbc2d5bd375995c72d7f63
Estherbyamasu <estherbyamasufuraha@gmail.com>
2020-12-18 02:28:53 -0800

    update in sidebar

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -57,8 +57,8 @@
           <li><a class="" href="{{ url('caisses')}}">
 						<span class="fa fa-bar-chart">&nbsp;</span> Caisses
 					</a></li>
-          <li><a class="" href="{{ url('detaille_caisses')}}">
-						<span class="fa fa-bar-chart">&nbsp;</span> Detaille caisse
+          <li><a class="" href="{{ url('caisse_details')}}">
+						<span class="fa fa-bar-chart">&nbsp;</span> Caisse details
 					</a></li>
          
               
